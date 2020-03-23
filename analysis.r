50182a3ed3432dfa2142504abae3b23e9bf2abc1
Scott Anderson <perfect_disturbance@hotmail.com>
2020-03-23 19:37:58 +0000

    Work on analysis script.

--- analysis.r
+++ analysis.r
@@ -1,10 +1,44 @@
 library(RSQLite)
 
+#Connect to SQLite Db
 conn <- dbConnect(RSQLite::SQLite(), "./coronadata.db")
 
-countrydata <- dbGetQuery(conn, "SELECT JULIANDAY(a.day) - 2458870.5 AS nday, a.dcount FROM alldata a WHERE a.country LIKE 'China' AND a.name LIKE 'Confirmed' GROUP BY a.country, a.day")
+#Parameters
+country <- "US"
+
+lastzerodata <- dbGetQuery(conn, "
+SELECT
+    a.day,
+    SUM(a.dcount) AS dcount 
+FROM 
+    alldata a
+WHERE
+    a.country LIKE ? AND 
+    a.name LIKE 'Confirmed' AND
+    dcount = 0
+GROUP BY 
+    a.country, a.day
+ORDER BY a.day DESC
+LIMIT 1 ", params = c(country))
+print(lastzerodata)
+lastzerocasedate <- lastzerodata[1,1]
+print(lastzerocasedate)
+
+countrydata <- dbGetQuery(conn, "
+SELECT
+    JULIANDAY(a.day) - 2458870.5 AS days_since_outbreak,
+    SUM(a.dcount) AS confirmed_cases 
+FROM 
+    alldata a
+WHERE
+    a.name LIKE 'Confirmed' AND
+    a.country LIKE ? AND 
+    a.day >= date(?)
+GROUP BY 
+    a.country, a.day", params = c(country, lastzerocasedate))
 
 print(countrydata)
-countrymodel <- nls(dcount ~ SSlogis(nday, Asym, xmid, scal), countrydata)
+countrymodel <- nls(confirmed_cases ~ SSlogis(days_since_outbreak, Asym, xmid, scal), countrydata)
+print(countrymodel)
 plot(countrydata)
-lines(countrydata$nday, predict(countrymodel))
+lines(countrydata$days_since_outbreak, predict(countrymodel))
